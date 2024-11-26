a0a01e772c31336e77c3dc38285a9497892de12f
Pablo Vinas <pablo.araujo@alu.ufc.br>
2024-11-26 13:05:48 -0300

    commit configurations

---
+++ src/main/java/com/restaurantepablo/restaurante/config/Configurations.java
@@ -0,0 +1,15 @@
+package com.restaurantepablo.restaurante.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
+
+@Configuration
+public class Configurations {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
